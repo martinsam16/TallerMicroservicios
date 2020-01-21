fe93e19427d242840ccbbba6c044f7553f27a010
MartinSamanArata2018 <martin.saman@vallegrande.edu.pe>
2020-01-21 12:49:37 -0500

    :fire: registrando metrica para contar las peticiones realizadas a un rest

--- tarea/src/main/java/com/martinsaman/tarea/rest/MetricasRest.java
+++ tarea/src/main/java/com/martinsaman/tarea/rest/MetricasRest.java
@@ -1,13 +1,19 @@
 package com.martinsaman.tarea.rest;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MetricasRest {
 
+    @Autowired
+    MeterRegistry registry;
+
     @GetMapping
     public String hola() {
-        return "Hola C:";
+        registry.counter("peticionesRest").increment();
+        return "Hola C: " + registry.get("peticionesRest").counter().count();
     }
 }
